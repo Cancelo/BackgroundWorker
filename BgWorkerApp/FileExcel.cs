05221e3fb0ad7e15ad8ce5b73717b25fe0c8f612
Cancelo <Cancelo@users.noreply.github.com>
2016-10-11 08:12:05 +0200

    Add files via upload

---
+++ BgWorkerApp/FileExcel.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace BgWorkerApp
+{
+    class FileExcel
+    {
+        static string name = "";
+        static string part = "";
+
+        public static string Read(string filePath, int row, int col, DateTime startDate, DateTime endDate)
+        {
+            string matrixValue = "";
+
+            if (!File.Exists(filePath))
+            {
+                matrixValue = "noFile";
+            }
+
+            var misValue = Type.Missing;
+            Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+            Excel.Workbook xlWorkBook = xlApp.Workbooks.Open(filePath, misValue, misValue,
+                                        misValue, misValue, misValue, misValue, misValue, misValue,
+                                        misValue, misValue, misValue, misValue, misValue, misValue);
+            Excel.Worksheet xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(2);
+            Excel.Range range = xlWorkSheet.UsedRange;
+
+            int totalRows = range.Rows.Count;
+
+            if ((range.Cells[row, col] as Excel.Range).Value != null)   // !=null almacena name
+            {
+                name = (range.Cells[row, col] as Excel.Range).Value.ToString().Replace(" ", "");
+            }
+
+            if ((range.Cells[row + 6, col] as Excel.Range).Value != null)   // !=null almacena part
+            {
+                part = (range.Cells[row + 6, col] as Excel.Range).Value.ToString();
+            }
+
+            int c = 0; // Contador
+            int c1 = 0; // Contador matchDate
+            for (int startRow = row + 7; startRow <= 539; startRow++)
+            {
+                if ((range.Cells[startRow, col] as Excel.Range).Value != null)
+                {
+                    c++;
+                    try
+                    {
+                        string match = (range.Cells[startRow, col] as Excel.Range).Value.ToString();
+                        DateTime matchDate = Convert.ToDateTime(match);
+
+                        // Comparación de fechas
+                        if (matchDate >= startDate && matchDate <= endDate)
+                        {
+                            c1++;
+                        }
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+            matrixValue = name + "^" + part + "^" + c1 + "^" + c;
+
+            // Cerrar
+            xlWorkBook.Close(false, misValue, misValue);
+            xlApp.Quit();
+
+            // Liberar
+            releaseObject(xlWorkSheet);
+            releaseObject(xlWorkBook);
+            releaseObject(xlApp);
+
+            return matrixValue;
+        }
+
+        public static void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unable to release the object(object:{0})", obj.ToString());
+            }
+            finally
+            {
+                obj = null;
+                GC.Collect();
+            }
+        }
+
+        public static void CreateReport(List<Course> listCourses)
+        {
+            foreach(Course s in listCourses)
+            {                
+                if(Convert.ToInt32(s.DateCount) != 0)
+                {
+                    Console.WriteLine(s.Name + " [" + s.Part + "]:   " + s.DateCount);
+                }                
+            }           
+        }        
+    }
+}
---
+++ BgWorkerApp/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace BgWorkerApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string filePath;
+        private int startCol;
+        private int totalCols;
+        private DateTime startDate;
+        private DateTime endDate;
+        private List<Course> listCourses = new List<Course>();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            startCol = Convert.ToInt32(startColTxt.Text);
+            totalCols = Convert.ToInt32(totalColsTxt.Text);
+            startDatePicker.SelectedDate = DateTime.Today;
+            endDatePicker.SelectedDate = DateTime.Today;
+            lblPath.Text = "Ningún archivo seleccionado";
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                filePath = openFileDialog.FileName;
+                    if(filePath != null)
+            {
+                btnDoAsynchronousCalculation.IsEnabled = true;
+                lblPath.Text = filePath;
+            }                        
+        }
+
+        private void btnDoAsynchronousCalculation_Click(object sender, RoutedEventArgs e)
+        {            
+            startDate = startDatePicker.SelectedDate.Value;
+            endDate = endDatePicker.SelectedDate.Value;
+            startCol = Convert.ToInt32(startColTxt.Text);
+            totalCols = Convert.ToInt32(totalColsTxt.Text);
+            pbCalculationProgress.Value = 0;
+            lbResults.Items.Clear();
+            lblPath.Text = "Leyendo archivo...";
+
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += worker_DoWork;
+            worker.ProgressChanged += worker_ProgressChanged;
+            worker.RunWorkerCompleted += worker_RunWorkerCompleted;
+            worker.RunWorkerAsync(Convert.ToInt32(totalColsTxt.Text));
+        }
+
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int max = (int)e.Argument;
+            int result = 0; // Contador            
+            string matrixValue;
+            int startRow = 7;
+
+            for (int col = startCol; col <= max; col++)
+            {
+                int progressPercentage = Convert.ToInt32(((double)col / max) * 100);
+                
+                matrixValue = FileExcel.Read(filePath, startRow, col, startDate, endDate);
+
+                if (matrixValue != null) { result++; }
+                
+                (sender as BackgroundWorker).ReportProgress(progressPercentage, matrixValue);
+                System.Threading.Thread.Sleep(1);
+            }
+            e.Result = result;
+        }
+
+        void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pbCalculationProgress.Value = e.ProgressPercentage;
+            lblPath.Text = "Leyendo archivo...  " + e.ProgressPercentage + "%";
+            {
+                string[] mV = e.UserState.ToString().Split('^');
+                listCourses.Add(new Course() { Name = mV[0], Part = mV[1], DateCount = mV[2], Total = mV[3]});
+                lbResults.ItemsSource = listCourses;
+            }
+                //lbResults.Items.Add(e.UserState);
+        }
+
+        void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            lblPath.Text = "Completado! " + e.Result + " Registros leidos";
+            btnCreateReport.IsEnabled = true;
+        }
+
+        private void btnCreateReport_Click(object sender, RoutedEventArgs e)
+        {
+            FileExcel.CreateReport(listCourses);
+        }
+
+        private void StackPanel_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+                this.DragMove();
+        }
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Window parentWindow = Window.GetWindow(this);            
+            App.Current.Shutdown();
+        }
+    }
+
+    class Course
+    {
+        private string name;
+        private string part;
+        private string dateCount;
+        private string total;
+
+        public string Name { get { return name; } set { name = value; } }
+        public string Part { get { return part; } set { part = value; } }
+        public string DateCount { get { return dateCount; } set { dateCount = value; } }
+        public string Total { get { return total; } set { total = value; } }
+    }
+}
