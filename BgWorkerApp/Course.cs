d843d9d97d650aceb13d4cdb7a0c274af8317fb4
Cancelo <Cancelo@users.noreply.github.com>
2016-10-11 08:20:56 +0200

    Add files via upload

---
+++ BgWorkerApp/Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BgWorkerApp
+{
+    class Course
+    {
+        private string name;
+        private string part;
+        private string dateCount;
+        private string total;
+
+        public string Name { get { return name; } set { name = value; } }
+        public string Part { get { return part; } set { part = value; } }
+        public string DateCount { get { return dateCount; } set { dateCount = value; } }
+        public string Total { get { return total; } set { total = value; } }
+    }
+}
